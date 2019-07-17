c234b15629dc623bc62fbed5e5354804e72603c4
rapuhotelli <jukka.lahetkangas@gmail.com>
2019-07-17 11:38:44 +0300

    Move to yarn workspaces to share node_modules, set up node js and typescript

---
+++ README.md
@@ -0,0 +1,15 @@
+# Here Be Dragons
+
+## Version 0 features
+
+### Main Screen app
+
+- insert tech specs
+
+### DM Screen app
+
+- insert tech specs
+
+### Broadcast server
+
+- insert tech specs
---
+++ hbt-server/src/index.ts
@@ -0,0 +1,12 @@
+
+import express from 'express';
+const app = express();
+const port = 8081;
+
+app.get( '/', ( req: Express.Request, res ) => {
+  res.send( 'Hello world yay' );
+} );
+
+app.listen( port, () => {
+  console.log( `server started at http://localhost:${ port }` );
+} );
