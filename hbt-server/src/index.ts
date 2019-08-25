d486a4b44ff12ec4a4946b7b9d6780f5a1490668
rapuhotelli <jukka.lahetkangas@gmail.com>
2019-08-25 10:51:13 +0300

    test a thing

--- hbt-server/src/index.ts
+++ hbt-server/src/index.ts
@@ -1,5 +1,6 @@
 
 import express from 'express';
+import fs from 'fs';
 import path from 'path';
 
 const app = express();
@@ -9,6 +10,29 @@ const pubdir = (uri: string): string => (path.join(__dirname, '..', 'public', ur
 
 app.get( '/', ( req: Express.Request, res ) => {
   res.sendFile(pubdir('index.html'));
+});
+
+app.get('/levelselect', (req: Express.Request, res) => {
+  const directoryPath = path.join(__dirname, '..', 'public', 'encounters', 'testcampaign');
+  let encounters = [];
+  fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+      return console.log('Unable to scan directory: ' + err);
+    }
+
+
+    encounters = files
+      .filter(file => file.endsWith('.json'))
+      .map(file => {
+        const encounterData = <string><unknown>fs.readFileSync(path.join(directoryPath, file));
+        return JSON.parse(encounterData);
+      });
+
+    res.send({
+      data: encounters,
+    });
+  });
 });
 
 app.use(express.static('public'));
